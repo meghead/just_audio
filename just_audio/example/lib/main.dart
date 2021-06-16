5e6d0c83c9763d0ffc3a118d66707f8ef6422f60
meghead <megthing@gmail.com>
2021-06-16 08:55:25 +0100

    Update main.dart

--- just_audio/example/lib/main.dart
+++ just_audio/example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import '../builders/JustPlayer.dart';
+import 'JustPlayer.dart';
 
 void main() => runApp(MaterialApp(home: PageViewDemo()));
 
